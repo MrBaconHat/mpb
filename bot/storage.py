c9b8d7218cbd6541ec2c4ebe2a00caebd762ce38
Mr_Hat <109857632+MrBaconHat@users.noreply.github.com>
2026-09-20 08:59:55 +0000

    Add is_enabled() to check module status without creating directories

--- bot/storage.py
+++ bot/storage.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from nestio.files import JSON
 from cachetools import TTLCache
 
@@ -24,3 +26,17 @@ class Storage:
             guild[module] = JSON(f"data/{guild_id}/{module}/{type}.json")
 
         return guild[module]
+
+    async def is_enabled(
+        self, 
+        guild_id: str | int, 
+        module: str
+    ) -> bool:
+        directory = Path(f"data/{guild_id}/{module}")
+        if not directory.is_dir:
+            return False
+
+        json = JSON(f"data/{guild_id}/{module}/config.json")
+        config_data = await json.get()
+
+        return config_data.get("is_enabled", False)
