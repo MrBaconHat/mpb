7896538b4c4e761a1871f731b7afafef57ec4c9a
Mr_Hat <109857632+MrBaconHat@users.noreply.github.com>
2026-09-20 09:03:38 +0000

    Remove unnecessary storage caching

--- bot/storage.py
+++ bot/storage.py
@@ -5,27 +5,14 @@ from cachetools import TTLCache
 
 
 class Storage:
-    def __init__(self):
-        self.guild_modules: dict[str, dict[str, JSON]] = TTLCache(maxsize=10000, ttl=3600)
-
+    
     def get_module(
         self,
         guild_id: int | str,
         module: str,
         type: str
     ) -> JSON:
-        guild_id = str(guild_id)
-        
-        guild = self.guild_modules.get(str(guild_id))
-
-        if guild is None:
-            guild = {}
-            self.guild_modules[str(guild_id)] = guild
-
-        if module not in guild:
-            guild[module] = JSON(f"data/{guild_id}/{module}/{type}.json")
-
-        return guild[module]
+        return JSON(f"data/{guild_id}/{module}/{type}.json")
 
     async def is_enabled(
         self, 
