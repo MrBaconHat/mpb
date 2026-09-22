02facb729ba5a9316f766c57e86c37ac1c9f1380
Mr_Hat <109857632+MrBaconHat@users.noreply.github.com>
2026-09-22 18:07:16 +0000

    Display modules descriptions in container

--- bot/cogs/module.py
+++ bot/cogs/module.py
@@ -42,7 +42,7 @@ class ModulePagination(ui.LayoutView):
             container.add_item(ui.Separator())
 
             toggle_section = ui.Section(
-                f"### {cog.MODULE_NAME}",
+                f"### {cog.MODULE_NAME}\n-# {cog.MODULE_DESCRIPTION}",
                 accessory=module_toggle_button
             )
 
