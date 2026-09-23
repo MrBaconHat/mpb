a664bc80b670ccd8d29649dfc3eebb2e50a3a0fb
Mr_Hat <109857632+MrBaconHat@users.noreply.github.com>
2026-09-23 15:47:54 +0000

    Add select menu for module navigation

--- bot/cogs/module.py
+++ bot/cogs/module.py
@@ -13,6 +13,7 @@ class Module(commands.Cog):
         self.MODULE_DESCRIPTION = module_description
         self.INTERNAL_NAME = internal_name
 
+    
     async def build_config_page(
         self,
         guild_id: str | int
@@ -21,51 +22,120 @@ class Module(commands.Cog):
 
 
 class ModuleView(ui.LayoutView):
-    def __init__(self, modules: list, module_config: dict, module_files: dict):
+    def __init__(
+        self,
+        guild_id: int,
+        modules: list, 
+        module_config: dict, 
+        module_files: dict
+    ):
         super().__init__()
 
+        self.guild_id = guild_id
         self.modules = modules
         self.module_config = module_config
         self.module_files = module_files
         
-        self.pages = []
-        self.current_page = 0
+        self.pages: dict[str, list[ui.Item]] = {}
+        self.selected_page = None
 
-        self.build_pages()
-
-        self.add_item(self.pages[self.current_page])
+        self.min = 0
+        self.max = 22  # Account for < and >
 
     
-    def build_pages(self):
+    async def initialize(self):
+        await self.build_pages()
+
+        container = ui.Container()
+
+        container.add_item(
+            ui.ActionRow(
+                self.build_select_menu()
+            )
+        )
+
+        if self.selected_page:
+            for item in self.pages[self.selected_page]:
+                container.add_item(item)
+
+        self.add_item(container)
+
+
+    def build_select_menu(self) -> ui.Select:
+        options = []
+
+        if self.min > 0:
+            options.append(
+                discord.SelectOption(
+                    label="<",
+                    description="Previous",
+                    value="back"
+                )
+            )
+
+        if self.selected_page is None and len(self.pages) > 0:
+            self.selected_page = list(self.pages)[0]
+
+        for module in self.modules[self.min:self.max]:
+            options.append(
+               discord.SelectOption(
+                   label=module.MODULE_NAME,
+                   description=module.MODULE_DESCRIPTION,
+                   value=module.INTERNAL_NAME,
+                   default=module.INTERNAL_NAME == self.selected_page
+                )
+            )
+
+        if self.max < len(self.modules):
+            options.append(
+                discord.SelectOption(
+                    label=">",
+                    description="Forward",
+                    value="forward"
+                )
+            )
+            
+        select = ui.Select(
+            placeholder="Select a module...",
+            options=options
+        )
+        select.callback = self.select_menu_callback
+        
+        return select
+    
+
+    async def build_pages(self):
         self.pages.clear()
         
-        for cog in self.modules:
-            container = ui.Container()
+        for module in self.modules:
+            components: list[ui.Item] = []
 
-            config = self.module_config.get(cog.INTERNAL_NAME, {})
+            config = self.module_config.get(module.INTERNAL_NAME, {})
+
             is_enabled = config.get("is_enabled", False)
             module_toggle_button = ui.Button(
                 emoji="<:toggle_off:1552010483710689391>" if not is_enabled else "<:toggle_on:1552010109285175356>",
-                custom_id=f"toggle:{cog.INTERNAL_NAME}"
+                custom_id=f"toggle:{module.INTERNAL_NAME}"
             )
             module_toggle_button.callback = self.toggle_button_callback
 
-            container.add_item(ui.TextDisplay("# Configuration"))
-
-            container.add_item(ui.Separator())
-
             toggle_section = ui.Section(
-                f"### {cog.MODULE_NAME}\n-# {getattr(cog, 'MODULE_DESCRIPTION', 'No description')}",
+                f"### {module.MODULE_NAME}\n-# {getattr(module, 'MODULE_DESCRIPTION', 'No description')}",
                 accessory=module_toggle_button
             )
 
-            container.add_item(toggle_section)
+            components.append(toggle_section)
+            components.append(ui.Separator())
 
-            self.pages.append(container)
-    
-    def update_page(self):
+            module_components = await module.build_config_page(self.guild_id) or []
+            for ui_item in module_components:
+                components.append(ui_item)
+
+            self.pages[module.INTERNAL_NAME] = components
+
+    async def update_page(self):
         self.clear_items()
-        self.add_item(self.pages[self.current_page])
+        await self.initialize()   
     
     async def toggle_button_callback(self, interaction: discord.Interaction):
         custom_id = interaction.data["custom_id"]
@@ -79,8 +149,29 @@ class ModuleView(ui.LayoutView):
 
         self.module_config[module_name] = await config.get() or {}
 
-        self.build_pages()
-        self.update_page()
+        await self.build_pages()
+        await self.update_page()
+
+        await interaction.response.edit_message(view=self)
+
+    async def select_menu_callback(self, interaction: discord.Interaction):
+        print("select callback")
+        value = interaction.data["values"][0]
+        print("value:", value)
+
+        if value == "back":
+            self.min -= 22
+            self.max -= 22
+
+        elif value == "forward":
+            self.min += 22
+            self.max += 22
+
+        else:
+            self.selected_page = value
+
+        await self.build_pages()
+        await self.update_page()
 
         await interaction.response.edit_message(view=self)
         
@@ -118,11 +209,14 @@ class ModuleManagement(commands.Cog):
             module_config[cog.INTERNAL_NAME] = await config.get() or {}
             module_files[cog.INTERNAL_NAME] = config
 
-        view = ModulePagination(
+        view = ModuleView(
+            i.guild.id,
             module, 
             module_config,
             module_files
         )
+        await view.initialize()
+        
         await i.followup.send(view=view, ephemeral=True)
 
 
